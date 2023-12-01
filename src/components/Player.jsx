1b7c4cf7967dd517fa79dab27b52e5c28c956791
Yelena Strizhakova <e.strizhakova@gmail.com>
2023-12-01 17:46:00 +0400

    fix: players input value

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -19,6 +19,7 @@ export default function Player({ initialName, symbol }) {
       <input
         required
         type="text"
+        value={playerName}
         onChange={handleChange}
       />
     );
