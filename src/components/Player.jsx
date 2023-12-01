348d76e3a35f3c30369148b202cd5a8834e202b8
Yelena Strizhakova <e.strizhakova@gmail.com>
2023-12-01 17:06:15 +0400

    feat: players

---
+++ src/components/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((isEditing) => !isEditing);
+  }
+
+  function handleChange(evt) {
+    setPlayerName(evt.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        required
+        type="text"
+        onChange={handleChange}
+      />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+      </span>
+    </li>
+  );
+}
